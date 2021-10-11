203990558b50ef254c3261fa84ec6e04ded5f0fd
Ubaidillah12 <92314077+Ubaidillah12@users.noreply.github.com>
2021-10-11 20:39:47 +0700

    Add files via upload

---
+++ src/simpleant/perkalian.java
@@ -0,0 +1,10 @@
+package simpleant;
+
+public class perkalian {
+   public static void main(String[] args) {
+       int a = 20,
+        b = 150, 
+        c = a*b;
+       System.out.println(c);
+   }
+}
